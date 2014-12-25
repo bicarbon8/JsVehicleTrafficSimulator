20e9f74965496fde50270b243d589d6e3fe92225
bicarbon8 <bicarbon8@gmail.com>
2014-12-25 22:32:49 +0000

    cleaning up build process and generating build with dependencies included

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,46 +11,41 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> v<%= pkg.version %>, created by: <%= pkg.author %> <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */\n',
         mangle: true,
         compress: true,
         sourceMap: true,
-        // beautify: true,
       },
       build: {
+        options: {
+          banner: '/*! <%= pkg.name %> v<%= pkg.version %>, created by: <%= pkg.author %> <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */\n'
+        },
         files: {
-          'dist/<%= pkg.main %>.min.js': [
-            'js/jsvts.js',
-            'js/objects/map.js',
-            'js/objects/plotter.js',
-            'js/objects/txtToMapParser.js',
-            'js/objects/utils.js',
-            'js/classes/stats.js',
-            'js/classes/movable.js',
-            'js/classes/renderable.js',
-            'js/classes/segment.js',
-            'js/classes/trafficFlowControl.js',
-            'js/classes/stopLight.js',
-            'js/classes/vehicle.js',
-            'js/classes/tempVehicle.js',
-            'js/classes/vehicleGenerator.js'
-          ]
+          'dist/<%= pkg.main %>.min.js': ['<%= pkg.scripts.base %>']
+        }
+      },
+      buildWithDeps: {
+        options: {
+          banner: '/*! <%= pkg.name %> v<%= pkg.version %>, created by: <%= pkg.author %> <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %>, includes three.js dependencies from: http://github.com/mrdoob/three.js/ */\n'
+        },
+        files: {
+          'dist/<%= pkg.main %>-deps.min.js': ['<%= pkg.scripts.dependencies %>','<%= pkg.scripts.base %>']
         }
       }
     },
-    copy: {
-      build: {
-        src: 'dist/<%= pkg.main %>.min.js',
-        dest: 'dist/<%= pkg.main %>-<%= pkg.version %>-<%= grunt.template.today("yyyymmddHHMMss") %>.min.js'
-      }
-    },
     qunit: {
-      all: ['tests/*.html']
+      all: ['<%= pkg.scripts.test %>']
+    },
+    copy: {
+      examples: {
+        expand: true,
+        cwd: 'dist/',
+        flatten: true,
+        src: '<%= pkg.main %>-deps.*', 
+        dest: 'examples/js/',
+        filter: 'isFile'
+      }
     }
   });
-
-  // Load the plugin that provides the "copy" task.
-  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -61,7 +56,10 @@ module.exports = function(grunt) {
   // This plugin provides the "qunit" task.
   grunt.loadNpmTasks('grunt-contrib-qunit');
 
+  // Load the plugin that provides the "copy" task.
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
   // Default task(s).
-  grunt.registerTask('default', ['clean','uglify','copy','qunit']);
+  grunt.registerTask('default', ['clean','uglify','qunit','copy']);
 
 };
